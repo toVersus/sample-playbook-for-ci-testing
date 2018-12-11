5018a294f04491a1e0337f62528ebd3bbabb5c47
toVersus <toversus2357@gmail.com>
2018-12-11 11:12:15 +0900

    add example test of using ansible variables in testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,25 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_use_ansible_var_from_get_variables(host):
+    all_variables = host.ansible.get_variables()
+    assert all_variables['httpd_port'] == 80
+
+
+def test_use_ansible_var_from_debug_var(host):
+    result = host.ansible("debug", "var=httpd_port")
+    assert result['httpd_port'] == 80
+
+
+def test_use_ansible_var_from_debug_msg(host):
+    result = host.ansible("debug", "msg={{ httpd_port }}")
+    assert result['msg'] == 80
+
+
+def test_use_ansible_var_from_role_defaults(host):
+    ansible_vars = host.ansible(
+        "include_vars", "file=../../roles/web/defaults/main.yml"
+    )["ansible_facts"]
+    assert ansible_vars['httpd_port'] == 8000
